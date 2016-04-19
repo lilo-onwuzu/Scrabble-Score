86a835bfe1e00aff45e2b81959028e35874a9863
John Klein and Lilo Onwuzu <lilo@>
2016-04-19 11:10:37 -0700

    GREEN for word

--- src/main/java/Scrabble.java
+++ src/main/java/Scrabble.java
@@ -4,32 +4,33 @@ public class Scrabble {
   public static void main (String[] args) {}
 
   public Integer scoreScrabble (String word) {
-  
-  String[] scrabbleArray = word.split("");
-  System.out.println(scrabbleArray[0]);
-  Integer score = 0;
-  for (Integer index=0; index < scrabbleArray.length; index++){
-      if (scrabbleArray[index] == "q" || scrabbleArray[index] == "z" ){
+
+    String[] scrabbleArray = word.split("");
+    Integer score = 0;
+    for (Integer index=0; index < scrabbleArray.length; index++){
+      System.out.println(scrabbleArray[index]);
+      if (scrabbleArray[index].equals("q") || scrabbleArray[index].equals("z") ){
         score += 10;
-      } else if (scrabbleArray[index] == "j" || scrabbleArray[index] == "x"){
+      } else if (scrabbleArray[index].equals("j") || scrabbleArray[index].equals("x")){
         score += 8;
-      } else if (scrabbleArray[index] == "k") {
+      } else if (scrabbleArray[index].equals("k")) {
         score += 5;
-      } else if (scrabbleArray[index] == "f" || scrabbleArray[index] == "h" || scrabbleArray[index] == "v" || scrabbleArray[index] == "w" || scrabbleArray[index] == "y") {
+      } else if (scrabbleArray[index].equals("f") || scrabbleArray[index].equals("h") || scrabbleArray[index].equals("v") || scrabbleArray[index].equals("w") || scrabbleArray[index].equals("y")) {
         score += 4;
-      } else if (scrabbleArray[index] == "b" || scrabbleArray[index] == "c" ||
-      scrabbleArray[index] == "m" || scrabbleArray[index] == "p") {
+      } else if (scrabbleArray[index].equals("b") || scrabbleArray[index].equals("c") ||
+      scrabbleArray[index].equals("m") || scrabbleArray[index].equals("p")) {
         score += 3;
-      } else if (scrabbleArray[index] == "d" || scrabbleArray[index] == "g"){
+      } else if (scrabbleArray[index].equals("d") || scrabbleArray[index].equals("g")){
         score += 2;
-      } else if (scrabbleArray[index] == "a" || scrabbleArray[index] == "e" ||
-      scrabbleArray[index] == "i" || scrabbleArray[index] == "o" ||
-      scrabbleArray[index] == "u" || scrabbleArray[index] == "l" ||
-      scrabbleArray[index] == "n" || scrabbleArray[index] == "r"||
-      scrabbleArray[index] == "s" || scrabbleArray[index] == "t") {
+      } else if (scrabbleArray[index].equals("a") || scrabbleArray[index].equals("e") ||
+      scrabbleArray[index].equals("i") || scrabbleArray[index].equals("o") ||
+      scrabbleArray[index].equals("u") || scrabbleArray[index].equals("l") ||
+      scrabbleArray[index].equals("n") || scrabbleArray[index].equals("r")||
+      scrabbleArray[index].equals("s") || scrabbleArray[index].equals("t")) {
         score++;
       }
     }
-  return score;
+    System.out.println(score);
+    return score;
   }
 }
