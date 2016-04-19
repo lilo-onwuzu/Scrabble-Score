35ea8c022ea527fc0d7a959691dfbaedf050763c
John Klein and Lilo Onwuzu <john@>
2016-04-19 10:26:16 -0700

    RED on score for eight point letter

--- src/test/java/ScrabbleTest.java
+++ src/test/java/ScrabbleTest.java
@@ -37,5 +37,11 @@ public class ScrabbleTest {
     Integer expected = 5;
     assertEquals(expected, testScrabble.scoreScrabble("k"));
   }
+  @Test
+  public void scoreScrabble_returnScoreForEightPointLetter_1(){
+    Scrabble testScrabble = new Scrabble();
+    Integer expected = 8;
+    assertEquals(expected, testScrabble.scoreScrabble("j"));
+  }
 
 }
