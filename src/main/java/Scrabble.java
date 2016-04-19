bee50308ddb21e901ac30c27c3b6f77675e3095d
John Klein and Lilo Onwuzu <john+lilo@>
2016-04-19 10:06:01 -0700

    Green on Score For One Point Letter

--- src/main/java/Scrabble.java
+++ src/main/java/Scrabble.java
@@ -1,7 +1,22 @@
+import java.util.ArrayList;
+
 public class Scrabble {
   public static void main (String[] args) {}
 
   public Integer scoreScrabble (String word) {
-    return 1;
+  // char[] scrabbleArray = word.toCharArray();
+  String[] scrabbleArray = word.split("");
+  System.out.println(scrabbleArray[0]);
+  Integer score = 0;
+  for (Integer index=0; index < scrabbleArray.length; index++){
+      if (scrabbleArray[index] == "a" || scrabbleArray[index] == "e" ||
+      scrabbleArray[index] == "i" || scrabbleArray[index] == "o" ||
+      scrabbleArray[index] == "u" || scrabbleArray[index] == "l" ||
+      scrabbleArray[index] == "n" || scrabbleArray[index] == "r"||
+      scrabbleArray[index] == "s" || scrabbleArray[index] == "t") {
+        score++;
+      }
+    }
+  return score;
   }
 }
